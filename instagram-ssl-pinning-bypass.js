7f4927fda48b128ac817350c55e3aef6d2daefa8
Eltion <eltimusa4@gmail.com>
2022-06-03 01:20:58 +0200

    Bugfix: wait for java before logging

--- instagram-ssl-pinning-bypass.js
+++ instagram-ssl-pinning-bypass.js
@@ -1,10 +1,10 @@
 'use strict'
 
-function hook_proxygen_SSLVerification(library){
+function hook_proxygen_SSLVerification(library) {
     const functionName = "_ZN8proxygen15SSLVerification17verifyWithMetricsEbP17x509_store_ctx_stRKNSt6__ndk112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEPNS0_31SSLFailureVerificationCallbacksEPNS0_31SSLSuccessVerificationCallbacksERKNS_15TimeUtilGenericINS3_6chrono12steady_clockEEERNS_10TraceEventE";
-    
+
     const f = Module.getExportByName(library.name, functionName);
-    if(!f){
+    if (!f) {
         console.error(`[*][*] Could not find function: ${functionName}`);
         return;
     }
@@ -17,10 +17,10 @@ function hook_proxygen_SSLVerification(library){
     logger(`[*][*] Hooked function: ${functionName}`);
 }
 
-function hook_X509_verify_cert(library){
+function hook_X509_verify_cert(library) {
     const functionName = "X509_verify_cert";
     const f = Module.getExportByName(library.name, functionName);
-    if(!f){
+    if (!f) {
         console.error(`[*][*] Could not find function: ${functionName}`);
         return;
     }
@@ -33,23 +33,24 @@ function hook_X509_verify_cert(library){
     logger(`[*][*] Hooked function: ${functionName}`);
 }
 
-async function waitForModule(moduleName){
+async function waitForModule(moduleName) {
     return new Promise(resolve => {
         const interval = setInterval(() => {
             const libliger = Process.findModuleByName(moduleName);
-            if(libliger != null){
+            if (libliger != null) {
                 clearInterval(interval);
                 resolve(libliger);
             }
-        },300);
+        }, 300);
     });
 }
 
-function logger(message)
-{
-	var Log = Java.use("android.util.Log");
-	console.log(message)
-	Log.v("SSL_PINNING_BYPASS", message);
+function logger(message) {
+    console.log(message);
+    Java.perform(function () {
+        var Log = Java.use("android.util.Log");
+        Log.v("SSL_PINNING_BYPASS", message);
+    });
 }
 
 
