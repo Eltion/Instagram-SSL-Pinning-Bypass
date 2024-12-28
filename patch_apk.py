8530b29c1fe89797a1abc6e44ec58be501dad09c
Eltion <eltimusa4@gmail.com>
2024-12-28 11:53:34 +0100

    Update patch script

--- patch_apk.py
+++ patch_apk.py
@@ -1,7 +1,6 @@
 import json
 import lzma
 import zipfile
-import lief
 from zipfile import ZipFile
 import shutil
 import os
@@ -11,17 +10,145 @@ import argparse
 from shutil import which
 import subprocess
 
-
 TEMP_FOLDER = os.getcwd() + "/temp"
 DEFAULT_OUTPUT_NAME = "app_patched.apk"
 SUPPORTED_ARCHS = ["x86","x86_64","armeabi-v7a","arm64-v8a"]
 
 
-def inject_frida_gadget(libpath):
-    print("Patching:", libpath)
-    libnative = lief.parse(libpath)
-    libnative.add_library("libgadget.so")
-    libnative.write(libpath)
+def _determine_end_of_smali_method_from_line(smali: list, start: int) -> int:
+        end_methods = [(i + start) for i, x in enumerate(smali[start:]) if '.end method' in x]
+
+        if len(end_methods) <= 0:
+            raise Exception('Unable to find the end of the existing constructor')
+
+        end_of_method = end_methods[0] - 1
+
+        if 'return' in smali[end_of_method]:
+            end_of_method -= 1
+
+        return end_of_method
+
+def _determine_smali_path_for_class(target_class) -> str:
+        target_class = target_class.replace('.', '/')
+
+        activity_path = os.path.join(TEMP_FOLDER, 'app', 'smali', target_class) + '.smali'
+        if not os.path.exists(activity_path):
+            for x in range(2, 100):
+                smali_path = os.path.join(TEMP_FOLDER, 'app', 'smali_classes{0}'.format(x))
+                if not os.path.exists(smali_path):
+                    break
+
+                activity_path = os.path.join(smali_path, target_class) + '.smali'
+
+                if os.path.exists(activity_path):
+                    break
+
+        if not os.path.exists(activity_path):
+            raise Exception('Unable to find smali to patch!')
+
+        return activity_path
+
+def _determine_first_inject_point_of_smali_method_from_line(smali: list, start: int) -> int:
+        pos = start
+        in_annotation = False
+        while pos + 1 < len(smali):
+            pos = pos + 1
+            line = smali[pos].strip()
+
+            if not line:
+                continue
+
+            if line.startswith(".locals "):
+                continue
+
+            if in_annotation or line.startswith(".annotation "):
+                in_annotation = True
+                continue
+
+            if line.startswith(".end annotation"):
+                in_annotation = False
+                continue
+
+            return pos - 1
+
+def _patch_smali_with_load_library(smali_lines: list, inject_marker: int) -> list:
+    full_load_library = ('.method static constructor <clinit>()V\n'
+                         '   .locals 0\n'  # _revalue_locals_count() will ++ this
+                         '\n'
+                         '   .prologue\n'
+                         '   const-string v0, "gadget"\n'
+                         '\n'
+                         '   invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n'
+                         '\n'
+                         '   return-void\n'
+                         '.end method\n')
+
+    partial_load_library = ('\n    const-string v0, "gadget"\n'
+                            '\n'
+                            '    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n')
+
+    if 'clinit' in smali_lines[inject_marker]:
+
+        inject_point = _determine_first_inject_point_of_smali_method_from_line(smali_lines, inject_marker)
+
+        patched_smali = \
+            smali_lines[:inject_point] + partial_load_library.splitlines(keepends=True) + \
+            smali_lines[inject_point:]
+
+    else:
+        patched_smali = \
+            smali_lines[:inject_marker] + full_load_library.splitlines(keepends=True) + smali_lines[inject_marker:]
+
+    return patched_smali
+
+def _revalue_locals_count(patched_smali: list, inject_marker: int):
+    end_of_method = _determine_end_of_smali_method_from_line(patched_smali, inject_marker)
+
+    defined_locals = [i for i, x in enumerate(patched_smali[inject_marker:end_of_method])
+                      if '.locals' in x]
+
+    if len(defined_locals) <= 0:
+        return patched_smali
+
+    locals_smali_offset = defined_locals[0] + inject_marker
+
+    try:
+        defined_local_value = patched_smali[locals_smali_offset].split(' ')[-1]
+        defined_local_value_as_int = int(defined_local_value, 10)
+        new_locals_value = defined_local_value_as_int + 1
+
+    except ValueError as e:
+        return patched_smali
+
+    patched_smali[locals_smali_offset] = patched_smali[locals_smali_offset].replace(
+        str(defined_local_value_as_int), str(new_locals_value))
+
+    return patched_smali
+
+# Function to inject a library into a target process.
+# Credits: This implementation is based on code from the objection project.
+# Repository: https://github.com/sensepost/objection
+def inject_load_library(mainClass: str):
+
+    smaliFile = _determine_smali_path_for_class(mainClass);
+
+    with open(smaliFile, 'r') as f:
+        smali_lines = f.readlines()
+
+    inject_marker = [i for i, x in enumerate(smali_lines) if '# direct methods' in x]
+
+    if len(inject_marker) <= 0:
+        raise Exception('Unable to determine position to inject a loadLibrary call')
+
+    inject_marker = inject_marker[0] + 1
+
+    patched_smali = _patch_smali_with_load_library(smali_lines, inject_marker)
+    patched_smali = _revalue_locals_count(patched_smali, inject_marker)
+
+    print("Patched smali code: " + smaliFile)
+
+    with open(smaliFile, 'w') as f:
+        f.write(''.join(patched_smali))
 
 
 def create_temp_folder():
@@ -41,7 +168,20 @@ def check_tools():
     if not is_tool_installed("zipalign"):
         print("zipalign not installed or not in PATH")
         return False
+    if not is_tool_installed("apktool"):
+        print("apktool not installed or not in PATH")
+        return False
     return True
+
+def unpack_apk(apk):
+    out = os.path.join(TEMP_FOLDER, "app")
+    subprocess.call('apktool d -r -f {0} -o {1}'.format(apk, out), shell=True)
+
+def pack_apk():
+    input = os.path.join(TEMP_FOLDER, "app")
+    output = os.path.join(TEMP_FOLDER, "new_app.apk")
+    subprocess.call('apktool b {0} -o {1}'.format(input, output), shell=True)
+    return output
 
 def create_keystore(keyalias, storepass):
     print("Generating keystore...")
@@ -86,18 +226,6 @@ def get_app_arch(apk):
                 res.append("arm64-v8a")
     return res
 
-
-def extract_libs_for_apk(apk, arch):
-    libs = ["libmemalign16.so","libfb.so"]
-    with ZipFile(apk) as zip_file:
-        namelist = zip_file.namelist()
-        for lib in libs:
-            libname = "lib/{0}/{1}".format(arch, lib)
-            if libname in namelist:
-                print("Extracting:", libname)
-                return zip_file.extract(libname, TEMP_FOLDER)
-
-
 def get_arch(apk):
     app_archs = get_app_arch(apk)
     print("App ABIs: ", app_archs)
@@ -134,7 +262,7 @@ def download_file(url, filename):
 
 
 def extract_frida_gadget(archive_path, arch):
-    filepath = os.path.join(TEMP_FOLDER, "lib", arch, "libgadget.so")
+    filepath = os.path.join(TEMP_FOLDER, "app" ,"lib", arch, "libgadget.so")
     with lzma.open(archive_path, mode='rb') as archive:
         with open(filepath, "wb") as f:
             f.write(archive.read())
@@ -145,10 +273,9 @@ def extract_frida_gadget(archive_path, arch):
 
 def download_frida_gadget(arch, version):
     arch_config = {
-        "x86":"x86",
-        "x86_64":"x86_64",
         "armeabi-v7a":"arm",
-        "arm64-v8a": "arm64"
+        "arm64-v8a": "arm64",
+        "x86": "x86"
     }
     response = requests.get(
         "https://api.github.com/repos/frida/frida/releases").text
@@ -183,13 +310,11 @@ def patch_apk(apk):
     apk_out.close()
     return apk_out.filename
 
-
 def copy_script_temp():
     src = os.path.join(os.getcwd(), "instagram-ssl-pinning-bypass.js")
     # added lib prefix and so extension so script is copied into /data/data/com.instagram.android/lib
     dest = os.path.join(TEMP_FOLDER, "libsslbypass.js.so")
     return shutil.copy(src, dest)
-
 
 def create_config_file():
     filepath = os.path.join(TEMP_FOLDER, "libgadget.config.so")
@@ -244,24 +369,26 @@ def main():
     else:
         keystore = create_keystore(keyalias, storepass)
 
+    unpack_apk(temp_apk)
+    #inject_load_library("com.instagram.mainactivity.LauncherActivity")
+    inject_load_library("com.instagram.service.tigon.configs.IGTigonConfig")
+
     config_file = create_config_file()
     print("Created config_file at: ", config_file)
     script = copy_script_temp()
     print("Created script_file at: ", script)
     for arch in archs:
         print("\nPatching for", arch)
-        nativelib = extract_libs_for_apk(temp_apk, arch)
-        arch_folder = os.path.join(TEMP_FOLDER, "lib", arch)
+        arch_folder = os.path.join(TEMP_FOLDER, "app", "lib", arch)
         download_frida_gadget(arch, frida_version)
-        inject_frida_gadget(nativelib)
         shutil.copy(config_file, arch_folder)
         shutil.copy(script, arch_folder)
-    output = patch_apk(temp_apk)
+
+    output = pack_apk()
     zip_align_apk(output)
     sign_apk(output, keystore, keyalias, storepass)
     outputpath = shutil.move(output, outputfile)
     delete_temp_folder()
     print("Sucessful. Patched file at:", outputpath)
-
 
 main()
